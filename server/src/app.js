de77b90d0d7482765c74328a323dd948bca5dda6
masudul alam <masudulalam972@gmail.com>
2024-10-16 09:58:16 +0600

    updates in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN ,
     credentials: true
 }));
 
