1271ce1e0ca343b94e1c57b0e79bcb384f539cf9
masudul alam <masudulalam972@gmail.com>
2024-10-16 19:55:31 +0600

    updates in app.js server file increase 16kb to 50mb

--- server/src/app.js
+++ server/src/app.js
@@ -11,8 +11,8 @@ app.use(cors({
 }));
 
 // Middleware to handle JSON and URL-encoded data
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 // Serve static files from the "public" directory
 app.use(express.static("public"));
