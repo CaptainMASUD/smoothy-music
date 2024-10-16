1c6635db1f7d60ef441325709f84bde5e1226940
masudul alam <masudulalam972@gmail.com>
2024-10-16 21:09:57 +0600

    change in app js

--- server/src/app.js
+++ server/src/app.js
@@ -11,8 +11,8 @@ app.use(cors({
 }));
 
 // Middleware to handle JSON and URL-encoded data
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "50MB" }));
+app.use(express.urlencoded({ extended: true, limit: "50MB" }));
 
 // Serve static files from the "public" directory
 app.use(express.static("public"));
